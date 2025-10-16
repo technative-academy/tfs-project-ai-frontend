c2a8c789bd5516b7caf8228fc0e02b37f850c493
timtim40a <timtim40a@gmail.com>
2025-10-16 17:28:39 +0100

    Show more/less functioning

---
+++ js/hideContent.js
@@ -0,0 +1,45 @@
+class HideContent {
+    constructor(contentWrapper, showMoreWrapper) {
+        this.contentWrapper = contentWrapper
+        this.showMoreWrapper = showMoreWrapper
+    }
+
+    hideContent(event = null) {
+        if (this.contentWrapper.childElementCount > 6) {
+            Array.from(this.contentWrapper.children).forEach((child, index) => {
+                if (index >= 6) {
+                    child.classList.add('products__hidden-item')
+                }
+            })
+
+            if (event == null) {
+                const showMoreButton = document.createElement('button')
+                showMoreButton.textContent = 'Show more'
+                showMoreButton.classList.add('show__more__button')
+                showMoreButton.addEventListener('click', (event) =>
+                    this.showContent(event)
+                )
+                this.showMoreWrapper.appendChild(showMoreButton)
+            } else {
+                event.currentTarget.classList.remove('hidden__content')
+                event.currentTarget.textContent = 'Show more'
+                event.currentTarget.addEventListener('click', (event) =>
+                    this.showContent(event)
+                )
+            }
+        }
+    }
+
+    showContent(event) {
+        const showMoreButton = event.currentTarget
+        console.log(this.contentWrapper.children)
+        Array.from(this.contentWrapper.children).forEach((element) => {
+            element.classList.remove('products__hidden-item')
+        })
+        showMoreButton.textContent = 'Show less'
+        showMoreButton.addEventListener('click', (event) =>
+            this.hideContent(event)
+        )
+    }
+}
+export default HideContent
