691191e8bc31890e5338c691ed47e98e36958e3c
Nathan Hor <nathanhor2001@gmail.com>
2025-10-16 16:45:10 +0100

    Make more changes to the about sectiion, adding new script called button.js to make the switching from one tab to another within the about section. updated the icons in the other pages and also the text from ask the brains to career nest as well

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-import ask from "./ask.js";
-import shop from "./shop.js";
+import ask from './ask.js'
+import shop from './shop.js'
+import button from './button.js'
 
-ask.init();
-shop.init();
+ask.init()
+shop.init()
+button.init()
