ba815582d0ce7fda8749516903bacc7792989555
Ravindgowda <raaavind@gmail.com>
2025-10-17 01:35:54 +0100

    commit #1 about page hamburger menu implemented on mobile mode and I've made a small change of removing the border line below nav options on desktop mode.

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@ import shop from './shop.js'
 
 ask.init()
 shop.init()
+
+// Hamburger-menu toggle
+const hamMenu = document.querySelector('.ham-menu')
+const offScreenMenu = document.querySelector('.off-screen-menu')
+
+hamMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active')
+    offScreenMenu.classList.toggle('active')
+})
