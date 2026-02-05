70d270ceb11b4719d756bce2829447b197e959c1
j0lol <me@j0.lol>
2026-02-05 15:56:18 +0000

    Clean up about page code

--- js/about.js
+++ js/about.js
@@ -1,14 +1,24 @@
-const tabs = document.querySelectorAll(".about__tab");
-const panels = document.querySelectorAll(".about__panel");
+class About {
 
-tabs.forEach((tab) => {
-  tab.addEventListener("click", () => {
-    tabs.forEach((t) => t.classList.remove("about__tab--active"));
-    panels.forEach((p) => p.classList.remove("about__panel--active"));
+  constructor() {
+    this.tabs = document.querySelectorAll(".about__tab");
+    this.panels = document.querySelectorAll(".about__panel");
+  }
 
-    tab.classList.add("about__tab--active");
-    document
-      .getElementById(tab.dataset.tab)
-      .classList.add("about__panel--active");
-  });
-});
+  init() {
+    this.tabs.forEach((tab) => {
+      tab.addEventListener("click", () => {
+        this.tabs.forEach((t) => t.classList.remove("about__tab--active"));
+        this.panels.forEach((p) => p.classList.remove("about__panel--active"));
+
+        tab.classList.add("about__tab--active");
+        document
+          .getElementById(tab.dataset.tab)
+          .classList.add("about__panel--active");
+      });
+    });
+    
+  }
+}
+
+export default new About();
