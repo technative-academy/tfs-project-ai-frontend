5bb62f9734b059b18e77c99357b705f5c5ebfb25
Amro <amro.youssef@ymail.com>
2026-02-05 11:37:14 +0000

    Finish about page and add content to tabs

--- js/about.js
+++ js/about.js
@@ -1,16 +1,41 @@
 class About {
   constructor() {
-    
+    this.tabsContainer = document.querySelector(".tabs_container")
+    this.aboutContainer = document.querySelector(".about_container")
+    if (this.tabsContainer) {
+        this.aboutAppButton = this.tabsContainer.querySelector(".about-app")
+        this.aboutTeamButton = this.tabsContainer.querySelector(".about-team")
+        
+    }
+    if (this.aboutContainer) {
+        this.aboutAppContent = this.aboutContainer.querySelector(".about-app-content")
+        this.aboutTeamContent = this.aboutContainer.querySelector(".about-team-content")
+    }
   }
   
 
   init() {
-    if (!this.searchContainer) return;
+    if (!this.tabsContainer) return;
+
+    this.aboutAppButton.addEventListener("click", () => this.onAppButtonPress())
+    this.aboutTeamButton.addEventListener("click", () => this.onTeamButtonPress())
     
   }
 
+  onAppButtonPress() {
+    this.aboutAppButton.classList.add("tab_selected");
+    this.aboutTeamButton.classList.remove("tab_selected");
+    this.aboutAppContent.style.display = "block";
+    this.aboutTeamContent.style.display = "none";
+  }
 
+  onTeamButtonPress() {
+    this.aboutAppButton.classList.remove("tab_selected");
+    this.aboutTeamButton.classList.add("tab_selected");
+    this.aboutTeamContent.style.display = "block";
+    this.aboutAppContent.style.display = "none";
 
+  }
   
 }
 
