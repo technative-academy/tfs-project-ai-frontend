efe6717938442187b241f895d0682718c37f7b42
Amro <amro.youssef@ymail.com>
2026-02-05 11:03:31 +0000

    start working on about page

---
+++ js/about.js
@@ -0,0 +1,18 @@
+class About {
+  constructor() {
+    
+  }
+  
+
+  init() {
+    if (!this.searchContainer) return;
+    
+  }
+
+
+
+  
+}
+
+// Expose an instance of the 'Shop' class
+export default new About();
