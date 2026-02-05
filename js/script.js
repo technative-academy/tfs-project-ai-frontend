0c8e2bb75e336dc79fd58ac477d375c57f7c3cca
Alex <alex_mcgarry@hotmail.co.uk>
2026-02-05 15:30:13 +0000

    update header

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 import ask from "./ask.js";
 import shop from "./shop.js";
 
+// Hamburger menu toggle
+const menuToggle = document.querySelector(".header__menu-toggle");
+const nav = document.querySelector(".header__nav");
+
+if (menuToggle && nav) {
+  menuToggle.addEventListener("click", () => {
+    const isOpen = nav.classList.toggle("header__nav--open");
+    menuToggle.classList.toggle("header__menu-toggle--active");
+    menuToggle.setAttribute("aria-expanded", isOpen);
+  });
+
+  // Close menu when clicking a link (mobile)
+  nav.querySelectorAll(".header__link").forEach((link) => {
+    link.addEventListener("click", () => {
+      nav.classList.remove("header__nav--open");
+      menuToggle.classList.remove("header__menu-toggle--active");
+      menuToggle.setAttribute("aria-expanded", "false");
+    });
+  });
+}
+
 ask.init();
 shop.init();
